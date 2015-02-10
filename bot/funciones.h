f7f30b614ee148d89b85ae91d4dae07b8242fafa
AlejandroCatalina <alecatfel@gmail.com>
2015-02-10 20:32:27 +0100

    Create funciones.h

---
+++ bot/funciones.h
@@ -0,0 +1,26 @@
+#ifndef _SOCKET_H
+#define _SOCKET_H
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+int abrirSocketTCP();
+int abrirSocketUDP();
+int abrirBind(int sockfd,const struct sockaddr_in *addr);
+int aceptar(int sockfd, struct sockaddr_in ip4addr);
+int abrirConnect(int sockfd, struct sockaddr ip4addr);
+int abrirListen(int sockfd);
+int recibir(int sockfd,char *buf);
+int escribir(int sockfd,char *msg);
+void * Ping(void *args);
+void * servRecv(void *args);
+
+
+int sockfd;
+#endif
